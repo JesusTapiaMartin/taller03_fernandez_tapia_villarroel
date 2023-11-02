2f1e372608658a3380ca4b88a55fe7289858a2c7
GatoMiau03 <d.benaldo03@ufromail.cl>
2023-11-02 10:30:19 -0300

    Commit codigo

--- src/main/java/Pack.java
+++ src/main/java/Pack.java
@@ -1,7 +1,20 @@
 
 public class Pack {
     // ===== ATRIBUTOS =====
+    Producto[][] producto;
 
+    public Pack(Producto[][] producto) {
+        this.producto = new Producto[1][20];
+    }
 
+    public Producto[][] getProducto() {
+
+        return producto;
+    }
+
+    public void setProducto(Producto[][] producto) {
+
+        this.producto = producto;
+    }
 
 }
